a8ee43f1a943cb88c4a051c1c8ea209bc97c5a4c
Louis Popi <theh2o64@gmail.com>
2016-03-26 16:34:07 +0100

    lettuce: Use f2fs on cache and data

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,7 @@ BOARD_USES_QCOM_HARDWARE := false
 # Recovery
 TARGET_RECOVERY_FSTAB := device/yu/lettuce/ramdisk/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
